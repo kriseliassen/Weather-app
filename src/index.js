c3bed5833b7a38539381c1fcdc437e64c1743860
Kriskriskris <kristin.eliassen@gmail.com>
2020-09-13 14:01:04 +0200

    Replaced CSS grid of HTML with flexbox

--- src/index.js
+++ src/index.js
@@ -1,3 +1,138 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `Last updated on ${day} at ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+function formatIcon(iconCode) {
+  let newWeatherIcon = iconCode;
+
+  if (newWeatherIcon === "01d") {
+    return "images/012-sun.png";
+  } else if (newWeatherIcon === "01n") {
+    return "images/013-halfmoon.png";
+  } else if (newWeatherIcon === "02d") {
+    return "images/007-cloudsandsun.png";
+  } else if (newWeatherIcon === "02n") {
+    return "images/002-cloudynight.png";
+  } else if (
+    newWeatherIcon === "03d" ||
+    newWeatherIcon === "04d" ||
+    newWeatherIcon === "03n" ||
+    newWeatherIcon === "04n"
+  ) {
+    return "images/009-cloud.png";
+  } else if (newWeatherIcon === "09d" || newWeatherIcon === "09n") {
+    return "images/023-cloud.png";
+  } else if (newWeatherIcon === "10d" || newWeatherIcon === "10n") {
+    return "images/006-cloud.png";
+  } else if (newWeatherIcon === "11d" || newWeatherIcon === "11n") {
+    return "images/018-lightning.png";
+  } else if (newWeatherIcon === "13d" || newWeatherIcon === "13n") {
+    return "images/010-snow.png";
+  } else if (newWeatherIcon === "50d" || newWeatherIcon === "50n") {
+    return "images/028-clouds.png";
+  } else {
+    return "images/004-cloudsandsun.png";
+  }
+}
+
+function displayWeather(response) {
+  let temperatureElement = document.querySelector("#temperature-now");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let maxTempElement = document.querySelector("#max-temp");
+  let minTempElement = document.querySelector("#min-temp");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  let newIcon = response.data.weather[0].icon;
+  let newIconElement = formatIcon(newIcon);
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src", `${newIconElement}`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector(".forecast");
+  forecastElement.innerHTML = null;
+  let forecast = 0;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    let newIcon = forecast.weather[0].icon;
+    let newIconElement = formatIcon(newIcon);
+    console.log(newIcon);
+    console.log(newIconElement);
+
+    forecastElement.innerHTML += `<section>
+            <h4>${formatHours(forecast.dt * 1000)}</h4>
+            <img src=${newIconElement} alt="" />
+            <span id="forecast-temperature">${Math.round(
+              forecast.main.temp
+            )}°</span>
+          </section>`;
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#input-city");
+  console.log(cityInputElement.value);
+  searchCity(cityInputElement.value);
+}
+
 function getCurrentPosition(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(currentLocation);
@@ -5,96 +140,52 @@ function getCurrentPosition(event) {
 
 function currentLocation(position) {
   let apiKey = "3f134ca2a6c6070d8c666bc9f960b659";
-  let units = "metric";
-  let apiPositionUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=${units}&appid=${apiKey}`;
+  let apiPositionUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`;
 
-  axios.get(apiPositionUrl).then(showWeather);
-}
+  axios.get(apiPositionUrl).then(displayWeather);
 
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#input-city").value;
-  searchCity(city);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function searchCity(city) {
   let apiKey = "3f134ca2a6c6070d8c666bc9f960b659";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}`;
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showWeather);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}`).then(displayWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
   let searchInput = document.querySelector("#input-city");
   searchInput.value = "";
 }
 
-function showWeather(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temp-now").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#weather-now").innerHTML =
-    response.data.weather[0].description;
-}
-
-function showFahrenheit(event) {
+function displayFahrenheitTemperature(event) {
   event.preventDefault();
-  let actualTempSelector = document.querySelector("#temp-now");
-  let actualTemp = Math.round((celsiusTemperature * 9) / 5 + 32);
-  actualTempSelector.innerHTML = actualTemp;
-  let actualTempType = document.querySelector("#c-f-marking");
-  actualTempType.innerHTML = "°F";
+  let temperatureElement = document.querySelector("#temperature-now");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-function showCelsius(event) {
+function displayCelsiusTemperature(event) {
   event.preventDefault();
-  let actualTempSelector = document.querySelector("#temp-now");
-  actualTempSelector.innerHTML = celsiusTemperature;
-  let actualTempType = document.querySelector("#c-f-marking");
-  actualTempType.innerHTML = "°C";
+  let temperatureElement = document.querySelector("#temperature-now");
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-let searchCityForm = document.querySelector("#search-city-form");
+let celsiusTemperature = null;
+searchCity("Stavanger");
+
+let searchCityForm = document.querySelector(".search-city-form");
 searchCityForm.addEventListener("submit", handleSubmit);
 
-let currentLocationButton = document.querySelector("#current-location-button");
+let currentLocationButton = document.querySelector("#current-location-btn");
 currentLocationButton.addEventListener("click", getCurrentPosition);
 
-let celsiusTemperature = 21;
-
-let actualFahrenheit = document.querySelector("#fahrenheit");
-actualFahrenheit.addEventListener("click", showFahrenheit);
-
-let actualCelsius = document.querySelector("#celsius");
-actualCelsius.addEventListener("click", showCelsius);
-
-let now = new Date();
-let hour = now.getHours();
-let minute = now.getMinutes();
-let date = now.getDate();
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-
-let rightNow = document.querySelector("#right-now");
-rightNow.innerHTML = `Right now, ${day} ${month} ${date} at ${hour}:${minute}, it's`;
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
